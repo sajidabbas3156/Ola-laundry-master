d1dc02948851b748fde0c522daaeac4e7b33edde
sajidabbas3156 <149969333+sajidabbas3156@users.noreply.github.com>
2025-09-29 19:39:24 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,8 +0,0 @@
-{pkgs}: {
-  deps = [
-    pkgs.python311Packages.tldextract
-    pkgs.python311Packages.ipyxact
-    pkgs.haskellPackages.string-variants
-    pkgs.python312Packages.graphite-web
-  ];
-}
